039bf17f8ad15068e2e333961f4e0d5df25c40f2
FOFOkaboom <slangen.edwin@gmail.com>
2019-02-14 22:45:51 +0100

    sshd setup configured

--- src/gizmo.py
+++ src/gizmo.py
@@ -2,6 +2,8 @@ import configparser
 from gizmo_email import EmailClient
 from gizmo_dropbox import DropboxClient
 from gizmo_initializer import Initializer
+
+
 class Main(object):
     """
     Main class
@@ -13,16 +15,19 @@ class Main(object):
         self.Config = configparser.ConfigParser()
         self.Config.read("config")
         self.debug = self.Config.get('options', 'debug')
+        #TODO write a config verification function
+        #TODO write a logger class
 
 
     # The email client
     #message_lst = ['test', 'this is a test message']
     #EmailClient().notification_message(message_lst)
 
-    # The SSHD initialiser
-    #Initializer().setup_sshd(sudopassword=input("Input your sudo password: "))   # Use this in prod
-    Initializer().setup_sshd(sudopassword='thisisnotmypassword')  # Unsafe, do not use in prod
+    # The SSHD initializer
+    #Initializer().setup_sshd()
 
+    # The SSHD configuration
+    #Initializer().config_sshd()
 
 if __name__ == "__main__":
     Main()
--- src/gizmo_initializer.py
+++ src/gizmo_initializer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import configparser
 import os
+import time
 
 class Initializer(object):
     """
@@ -15,30 +16,72 @@ class Initializer(object):
         self.Config.read("config")
         #TODO Modify debug value, is str should be boolean.
         self.debug = True
+        self.ssh_port = self.Config.get('sshd', 'port')
+        self.ssh_interface = self.Config.get('sshd', 'interface')
+        #TODO Remove sudo_pw line below
+        self.sudo_pw = self.Config.get('auth', 'password')  # UNSAFE REPLACE AFTER DEBUG WITH LINE BELOW
+        # self.sudo_pw = input("Input sudo password: ")
 
-    def setup_sshd(self, sudopassword):
+    def config_sshd(self):
+        # Configure SSHD
+        # First check if a non default ssh_config file is used (based on multiple references of ListenAddress)
+        try:
+            with open("/etc/ssh/sshd_config", "r") as old_sshd_file:
+                if "\nListenAddress" in old_sshd_file.read():
+                    print("A non default sshd config file is found, a backup is saved to /etc/ssh/sshd_config.backup")
+                    command = "echo '{}' | sudo tee /etc/ssh/sshd_config.backup > /dev/null 2>&1".format(old_sshd_file)
+                    os.system('echo %s| sudo -S %s' % (self.sudo_pw, command))
+        except FileNotFoundError as e:
+            print(e)
+            pass
+
+        # Copy SSHD config
+        try:
+            with open('sshd_config', 'r') as new_sshd_file:
+                # Retrieve the sshd config settings
+                # Store the config file with sudo permissions and write stdout to /dev/null
+                command = "echo '{}' | sudo tee /etc/ssh/sshd_config > /dev/null 2>&1".format(
+                    new_sshd_file.read().format(self.ssh_port, self.ssh_interface))
+                os.system('echo %s| sudo -S %s' % (self.sudo_pw, command))
+        except FileNotFoundError as e:
+            print(e)
+
+    def setup_sshd(self):
         """
-        Installs and configures the SSHD server
+        Installs and configures the SSHD server.
         :return: void
         """
-        sudo_pw = self.Config.get('auth', 'password')  # UNSAFE REMOVE AFTER DEBUG
-        #sudo_pw = sudopassword
-
         # Update apt repository
-        command = 'apt update -y'
-        os.system('echo %s| sudo -S %s' % (sudo_pw, command))
+        command = 'apt update -y > /dev/null 2>&1'
+        if self.debug is True:
+            print('Executing apt update -y ')
+        try:
+            os.system('echo %s| sudo -S %s' % (self.sudo_pw, command))
+        except:
+            print("An error occured during 'apt update -u'")
 
         # Install ssh package
-        command = 'apt install ssh -y'
+        command = 'apt install ssh -y > /dev/null 2>&1'
+        if self.debug is True:
+            print('Executing apt install ssh -y')
         try:
-            os.system('echo %s| sudo -S %s' % (sudo_pw, command))
-        except OSError as e:
-            print('An error occured during "$ sudo apt install ssh -y" [{}]').format(e)
+            os.system('echo %s| sudo -S %s' % (self.sudo_pw, command))
+        except:
+            print("An error occured during 'apt install ssh -y' while installing ssh")
 
-        # Configure SSHD
-        # TODO Configure SSHD
-        # TODO autostart SSH service on boot
-        # TODO restart SSH service
+        # Configure sshd using the config
+        self.config_sshd()
+
+         # Reload sshd config
+        time.sleep(1)
+        try:
+            command = "service ssh restart > /dev/null 2>&1"
+            os.system('echo %s| sudo -S %s' % (self.sudo_pw, command))
+            print('SSHD_installed and configured successfully, SSHD listening on port {}'.format(self.ssh_port))
+        except:
+            print('An error occured during ssh "sudo service ssh reload" while installing ssh')
+
+
         # TODO Host based firewall activation/verification?
 
         # pkg_name = "ssh"
